265ee72ffaa29dda85fc061a7430e57ab0cbc1e4
Valss22 <valsshokorov@gmail.com>
2022-11-26 20:48:30 +0600

    Complete convert_currency()

--- airflow-service/app/api/airflows.py
+++ airflow-service/app/api/airflows.py
@@ -6,14 +6,24 @@ import xmltodict
 airflows = APIRouter()
 
 
-def convert_currency(title: str, amount: float) -> float:
+def convert_currency(from_currency: str, to_currency: str, amount: float) -> float:
+    if from_currency == to_currency:
+        return amount
     url = "https://www.nationalbank.kz/rss/get_rates.cfm?fdate=26.10.2021"
     response_xml = httpx.get(url)
     response_dict = xmltodict.parse(response_xml.text)
     currency_items = response_dict["rates"]["item"]
     for currency_item in currency_items:
-        if currency_item["title"] == title:
-            return round(float(currency_item["description"]) * amount, 2)
+        if currency_item["title"] == to_currency:
+            if from_currency == "KZT":
+                return round(amount / float(currency_item["description"]), 2)
+            to_currency_rate = float(currency_item["description"])
+        if currency_item["title"] == from_currency:
+            if to_currency == "KZT":
+                return round(float(currency_item["description"]) * amount, 2)
+            from_currency_rate = float(currency_item["description"])
+
+    return round(from_currency_rate / to_currency_rate * amount, 2)
 
 
 @airflows.post("/search")
@@ -33,10 +43,10 @@ async def airflow_results(search_id: str, currency: str):
             if value == search_id:
                 for j in range(len(data_el["items"])):
                     price_item = results[i]["items"][j]["price"]
-                    price_item["currency"] = currency
                     price_item["amount"] = convert_currency(
-                        currency, price_item["amount"]
+                        price_item["currency"], currency, price_item["amount"]
                     )
+                    price_item["currency"] = currency
                 return results[i]
         i += 1
     return {"detail": "not found"}
