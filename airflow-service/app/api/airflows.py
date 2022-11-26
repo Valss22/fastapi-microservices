e1bca4155cce129f5380e0f39a9dc54ff595cdd1
Valss22 <valsshokorov@gmail.com>
2022-11-26 20:52:00 +0600

    Add HOST_URL constants

--- airflow-service/app/api/airflows.py
+++ airflow-service/app/api/airflows.py
@@ -5,6 +5,9 @@ import xmltodict
 
 airflows = APIRouter()
 
+PROVIDER_A_HOST_URL = os.environ.get("PROVIDER_A_HOST_URL")
+PROVIDER_B_HOST_URL = os.environ.get("PROVIDER_B_HOST_URL")
+
 
 def convert_currency(from_currency: str, to_currency: str, amount: float) -> float:
     if from_currency == to_currency:
@@ -28,14 +31,14 @@ def convert_currency(from_currency: str, to_currency: str, amount: float) -> flo
 
 @airflows.post("/search")
 async def airflow_search():
-    provider_a_response = httpx.post(os.environ.get("PROVIDER_A_HOST_URL") + "/search")
-    provider_b_response = httpx.post(os.environ.get("PROVIDER_B_HOST_URL") + "/search")
+    provider_a_response = httpx.post(PROVIDER_A_HOST_URL + "/search")
+    provider_b_response = httpx.post(PROVIDER_B_HOST_URL + "/search")
     return provider_a_response.json()
 
 
 @airflows.get("/results/{search_id}/{currency}")
 async def airflow_results(search_id: str, currency: str):
-    provider_a_response = httpx.post(os.environ.get("PROVIDER_A_HOST_URL") + "/search")
+    provider_a_response = httpx.post(PROVIDER_A_HOST_URL + "/search")
     results = provider_a_response.json()
     i = 0
     for data_el in results:
