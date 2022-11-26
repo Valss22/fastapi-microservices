f9ee232c6ef0b0289f8592c64b434bb507ebd6ce
Valss22 <valsshokorov@gmail.com>
2022-11-26 12:25:12 +0600

    Add response_b.json & return this in provider_b

--- provider-b-service/app/api/providers_b.py
+++ provider-b-service/app/api/providers_b.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
+import json
+import asyncio
 
 providers_b = APIRouter()
 
 
 @providers_b.post("/search")
-def provider_b_search():
-    return {"detail": "Provider B search"}
+async def provider_b_search():
+    data = json.load(open("app/api/response_b.json", "r"))
+    await asyncio.sleep(60)
+    return data
