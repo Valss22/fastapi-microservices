e69d1d9e79fc7edff962fb37f722b5a188b26ec9
Valss22 <valsshokorov@gmail.com>
2022-11-27 15:08:13 +0600

    Add item req body optional

--- provider-a-service/app/api/providers_a.py
+++ provider-a-service/app/api/providers_a.py
@@ -2,15 +2,17 @@ from fastapi import APIRouter
 import json
 import asyncio
 from app.api.schemas import Item
+from typing import Optional
 
 providers_a = APIRouter()
 
 
 @providers_a.post("/search")
-async def provider_a_search(item: Item):
+async def provider_a_search(item: Optional[Item] = None):
     data = json.load(open("app/api/response_a.json", "r"))
-    data["items"].append(item.dict())
-    with open("app/api/response_a.json", "w") as file:
-        json.dump(data, file)
+    if item:
+        data["items"].append(item.dict())
+        with open("app/api/response_a.json", "w") as file:
+            json.dump(data, file)
     await asyncio.sleep(1)
     return data
--- provider-b-service/app/api/providers_b.py
+++ provider-b-service/app/api/providers_b.py
@@ -2,15 +2,17 @@ from fastapi import APIRouter
 import json
 import asyncio
 from app.api.schemas import Item
+from typing import Optional
 
 providers_b = APIRouter()
 
 
 @providers_b.post("/search")
-async def provider_a_search(item: Item):
+async def provider_a_search(item: Optional[Item] = None):
     data = json.load(open("app/api/response_b.json", "r"))
-    data["items"].append(item.dict())
-    with open("app/api/response_b.json", "w") as file:
-        json.dump(data, file)
+    if item:
+        data["items"].append(item.dict())
+        with open("app/api/response_b.json", "w") as file:
+            json.dump(data, file)
     await asyncio.sleep(2)
     return data
