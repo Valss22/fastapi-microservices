3ffc716b2d8df52e1a862c9498fb2f9d7f0b53cb
Valss22 <valsshokorov@gmail.com>
2023-02-15 01:39:57 +0600

    accepting changes from merged

--- airflow-service/app/main.py
+++ airflow-service/app/main.py
@@ -1,12 +1,9 @@
+from datetime import datetime
+
 from fastapi import FastAPI
-<<<<<<< HEAD
+
 from app.api.endpoints import airflow_router, download_currency_rates
 
-=======
-from app.api.airflows import airflows
-from app.api.airflows import download_currency_rates
-from fastapi_utils.tasks import repeat_every
->>>>>>> ba91c5a160ae04ff370c7412f876d5e12fefbae5
 
 app = FastAPI(
     openapi_url="/api/v1/airflows/openapi.json", docs_url="/api/v1/airflows/docs"
@@ -16,18 +13,6 @@ app = FastAPI(
 @app.on_event("startup")
 async def startup():
     download_currency_rates()
-
-
-<<<<<<< HEAD
-# @app.on_event("startup")
-# def downloads_currency_rates_every_day():
-#     import schedule
-=======
-@app.on_event("startup")
-@repeat_every(seconds=60)
-async def downloads_currency_rates_every_day():
-    from datetime import datetime
->>>>>>> ba91c5a160ae04ff370c7412f876d5e12fefbae5
 
     current_hour = datetime.now().strftime("%H")
     if current_hour == "12":
